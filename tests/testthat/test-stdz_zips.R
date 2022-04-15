bd98e9e2f742c66e44541c7e72b1601fef8edf79
parmsam <parmartsam@gmail.com>
2022-04-14 23:38:53 -0400

    reorder test order lines

--- tests/testthat/test-stdz_zips.R
+++ tests/testthat/test-stdz_zips.R
@@ -7,6 +7,6 @@ test_that("stdz_zips works to format 9 digit numbers", {
 
 test_that("stdz_zips works to format 5 digit numbers", {
   expected2 <- stdz_zips(90210)
-  expect_equal(expected2, "90210")
   expect_type(expected2, "character")
+  expect_equal(expected2, "90210")
 })
